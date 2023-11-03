005fc5595a1b50b0e93804cd2d98ca2acef541a2
dymafr <contact@dyma.fr>
2023-11-03 11:36:30 +0100

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './assets/styles/index.scss';
+import { ApiContext } from './context/ApiContext';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <ApiContext.Provider value="https://restapi.fr/api/recipes">
+      <RouterProvider router={router} />
+    </ApiContext.Provider>
+  </StrictMode>
+);
